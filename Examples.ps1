1e29d0e48f454a921090508d6ba5c5895e86b912
Rob Moore <me@robdmoore.id.au>
2015-05-01 15:48:20 +0800

    Added more examples and code to initialise with subscription id

--- Examples.ps1
+++ Examples.ps1
@@ -1,12 +1,32 @@
 ﻿Import-Module $PSScriptRoot\Set-ARMServicePrincipalCredential.psm1 -Force
 
-$SubscriptionId = Get-Content (Join-Path $PSScriptRoot "SubscriptionId.txt")
+
+$subscriptionIdFile = Join-Path $PSScriptRoot "SubscriptionId.txt"
+if (-not (Test-Path $subscriptionIdFile)) {
+    Write-Host "Enter subscription id to deploy into: "
+    Set-Content -Path $subscriptionIdFile Read-Host
+}
+
+$SubscriptionId = Get-Content $subscriptionIdFile
 
 $result = Set-ARMServicePrincipalCredential `
     -SubscriptionId $SubscriptionId `
     -AppName "MySecretApp" `
     -AppEnvironment "Production" `
-    -Location "Southeast Asia" `
+    -Location "Southeast Asia"
+    
+Write-Host $result
+
+$result = Set-ARMServicePrincipalCredential `
+    -SubscriptionId $SubscriptionId `
+    -ResourceGroupName "MyResourceGroup" `
+    -Location "Southeast Asia"
+    
+Write-Host $result
+
+$result = Set-ARMServicePrincipalCredential `
+    -SubscriptionId $SubscriptionId `
+    -AzureADAppName "MyAzureADApp" `
     -Verbose
 
 Write-Host $result
